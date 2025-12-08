9ff4b1dba33f975e79625047ef3401bcf63bfdf4
Apparaat Negen <apparaat9@gmail.com>
2025-12-08 10:25:17 +0100

    Day 08 25 obo

--- 2025/day_08.py
+++ 2025/day_08.py
@@ -1,20 +1,20 @@
 
-from math import prod
+from math import prod, dist
 from collections import defaultdict
 
 d = {tuple(map(int, x.split(','))) for x in open('./input/day_08.txt').read().splitlines()}
-distances = {(sum(map(lambda x : (x[0] - x[1])**2, zip(x,y))), *sorted((x, y))) for x in d for y in d if x != y}
+distances = {(dist(x,y), *sorted((x, y))) for x in d for y in d if x != y}
 circuits = {x : {x} for x in d}
 
-for i, v in enumerate(sorted(distances)):
-    new_circuit = circuits[v[1]] | circuits[v[2]]
+for i, (_, a, b) in enumerate(sorted(distances)):
+    new_circuit = circuits[a] | circuits[b]
 
     if len(new_circuit) == len(d):
-        print(v[1][0]*v[2][0])
+        print(a[0]*b[0])
         break
 
     for k in new_circuit:
         circuits[k] = new_circuit
 
-    if i == 1000:
+    if i == 999:
         print(prod(sorted(len(x) for x in {tuple(x) for _, x in circuits.items()})[-3:]))
