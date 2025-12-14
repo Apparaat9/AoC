d17cc16ca7f7c817ceab42e465455454e6441e56
Apparaat Negen <apparaat9@gmail.com>
2025-12-14 13:31:19 +0100

    Day 09 25 rf

--- 2025/day_09.py
+++ 2025/day_09.py
@@ -12,9 +12,7 @@ for (w1, h1), (w2, h2) in combinations(d, 2):
     h1, h2 = sorted([h1, h2])
     sp = Polygon([(w1+1,h1+1),(w1+1,h2-1),(w2-1,h1+1),(w2-1,h2-1)])
 
-    if p.contains(sp):
-        if area > r2: r2 = area
-
     if area > r1: r1 = area
+    if area > r2 and p.contains(sp): r2 = area
     
 print(r1, r2)
